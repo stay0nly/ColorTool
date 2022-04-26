e4dca3a4795e642be5f918c4addb2e03a1c42916
stay0nly <derkach.dmt@gmail.com>
2022-04-26 18:06:02 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const hexInput = document.getElementById('hexInput');
+const inputColor = document.getElementById('inputColor');
+const alteredColor = document.getElementById('alteredColor');
+
+hexInput.addEventListener('keyup', () => {
+    const hex = hexInput.value;
+    if(!isValidHex(hex)) return;
+
+    const strippedHex = hex.replace('#', '');
+    inputColor.style.backgroundColor = "#" + strippedHex;
+})
+
+
+const isValidHex = (hex) => {
+    if(!hex) return false;
+
+    const strippedHex = hex.replace('#', '');
+    return strippedHex.length === 3 || strippedHex.length === 6;
+}
