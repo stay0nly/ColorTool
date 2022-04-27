4ecccacbfc0e78a07dbc896483ad223279d7bd31
stay0nly <derkach.dmt@gmail.com>
2022-04-27 12:39:46 +0800

    Converting functionality added & slider range

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const hexInput = document.getElementById('hexInput');
 const inputColor = document.getElementById('inputColor');
 const alteredColor = document.getElementById('alteredColor');
 
+
 hexInput.addEventListener('keyup', () => {
     const hex = hexInput.value;
     if(!isValidHex(hex)) return;
@@ -16,4 +17,38 @@ const isValidHex = (hex) => {
 
     const strippedHex = hex.replace('#', '');
     return strippedHex.length === 3 || strippedHex.length === 6;
+}
+
+const convertHexToRgb = (hex) => {
+    if(!isValidHex(hex)) return null;
+
+    const strippedHex = hex.replace('#', '');
+
+    if(strippedHex.length === 3) {
+        strippedHex = strippedHex[0] + strippedHex[0]
+        + strippedHex[1] + strippedHex[1]
+        + strippedHex[2] + strippedHex[2]
+    }
+
+    const r = parseInt(strippedHex.substring(0,2), 16);
+    const g = parseInt(strippedHex.substring(2,4), 16);
+    const b = parseInt(strippedHex.substring(4,6), 16);
+
+    return {r,g,b}
+}
+
+const convertRgbToHex = (r,g,b) => {
+    const firstPair = ("0" + r.toString(16)).slice(-2);
+    const secondPair = ("0" + g.toString(16)).slice(-2);
+    const thirdPair = ("0" + b.toString(16)).slice(-2);
+
+    const hex = "#" + firstPair + secondPair + thirdPair;
+    return hex;
+}
+
+
+function range() {
+    const slidertext = document.getElementById('slidertext');
+    const slider = document.getElementById('slider');
+    slidertext.innerHTML = slider.value + "%";
 }
